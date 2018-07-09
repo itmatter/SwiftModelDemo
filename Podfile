e50b850fa3f2e679123797b2df0ac5651fa00808
liliguang <liliguang@liliguangdeiMac.local>
2018-07-09 11:46:28 +0800

    创建项目
    创建项目

---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SwiftModelDemo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SwiftModelDemo
+
+  target 'SwiftModelDemoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SwiftModelDemoUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+
+
+pod 'ObjectMapper'
+
+end
---
+++ SwiftModelDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftModelDemo
+//
+//  Created by liliguang on 2018/7/9.
+//  Copyright © 2018年 liliguang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
